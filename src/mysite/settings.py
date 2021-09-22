b49000f945e1c02c7cd2d840c52ee2ab7bb5f89b
Karim-Valeev <karim.valeev.i@gmail.com>
2021-09-23 00:03:32 +0300

    Fix settings

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'av*kw#6ha)1#l6rcx$2t9e^-jz9hj2p=fl#wqq09a71=+==&a3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-teamplanner.heroku.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://django-teamplanner.herokuapp.com/']
 
 
 # Application definition
