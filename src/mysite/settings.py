2958a2ac8e52cc97aea40425dd9395a1035053fc
Karim-Valeev <karim.valeev.i@gmail.com>
2021-09-22 23:54:40 +0300

    Fix requirements.txt and settings.py

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
@@ -133,5 +133,5 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+# # Activate Django-Heroku.
+# django_heroku.settings(locals())
